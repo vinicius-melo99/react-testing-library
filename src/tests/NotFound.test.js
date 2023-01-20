09b2a7098c84a8a93421e43685a63c07d1f23a38
Vinícius Melo <viniciusmelo317@gmail.com>
2023-01-20 17:01:35 -0300

    requisito 4 ok

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes do componente <NotFound.js />', () => {
+  it('A página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const NFTitle = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(NFTitle).toBeInTheDocument();
+  });
+
+  it('A página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByRole('img');
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
